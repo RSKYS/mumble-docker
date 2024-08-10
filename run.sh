16c462cc84e1dad3a2ee33a8030088a9d9768ac6
Pouria Rezaei <Pouria.rz@outlook.com>
2024-08-10 19:50:40 +0330

    Final fixes.
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if [ -d "$(pwd)/data" ]
+if [ -d "$(pwd)/data" ]; then
     chown -R 10000:10000 data
 else
-    echo -e "\n\
+    echo "\n\
     Hmm, we're probably not in right location..\n\
     Are you sure murmur docker stuffs are here?\n"
 
     exit 1
 fi
 
-if which docker-compose > /dev/null 2>&1 \
-            && [ -f "$(pwd)/Dockerfile" ] \
-            && [ -f "$(pwd)/docker-compose.y*ml" ]; then
+if command -v docker-compose > /dev/null 2>&1 \
+                    && [ -f "$(pwd)/Dockerfile" ] \
+                && [ -f "$(pwd)/docker-compose.yml" ]; then
     docker-compose down || true
     docker-compose build --no-cache
     docker-compose up --build --force-recreate -d
 else
-    echo -e "\n\
+    echo "\n\
     Where's docker-compose?\n\
     Install it first and come back later.\n"
 
